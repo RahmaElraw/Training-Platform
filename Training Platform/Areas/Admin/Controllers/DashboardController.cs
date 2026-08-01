97f2a74c817786452dbb60cd5c748fd9124539af
ahmed-elshenawy1414 <ahmedelshenawy1414@gmail.com>
2026-08-01 19:29:22 +0300

    Add DashboardController and update project file
    Added DashboardController with Index action in Admin area.
    Removed Areas and Areas\Admin\Controllers folder entries from Training Platform.csproj.

---
+++ Training Platform/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Training_Platform.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

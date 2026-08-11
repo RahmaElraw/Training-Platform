307e2d273e900e5ec7b10a7a444f6cd8f0f57f97
Rahma ElRaw <rahmaelraw67@gmail.com>
2026-08-12 00:07:48 +0300

    Secure email sender credentials

--- Training Platform/Utilities/EmailSender.cs
+++ Training Platform/Utilities/EmailSender.cs
@@ -6,17 +6,25 @@ namespace Training_Platform.Utilities
 {
     public class EmailSender : IEmailSender
     {
+        private readonly IConfiguration _configuration;
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var gmailEmail = _configuration["EmailSettings:Email"];
+            var gmailPassword = _configuration["EmailSettings:AppPassword"];
             var client = new SmtpClient("smtp.gmail.com", 587)
             {
                 EnableSsl=true,
                 UseDefaultCredentials=false,
-                Credentials = new NetworkCredential("rahmaelraw67@gmail.com", "cpdt rgxw xfmp uudq")
+                Credentials = new NetworkCredential(gmailEmail, gmailPassword)
             };
             return client.SendMailAsync(
                 new MailMessage(
-                    from:"rahmaelraw67@gmail.com",
+                    from: gmailEmail,
                     to:email,
                     subject,
                     htmlMessage
